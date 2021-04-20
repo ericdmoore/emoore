ca96f064b3e28ab2dca679741123e4a1ef183943
Eric Moore <eric.d.moore@gmail.com>
2021-04-20 17:00:13 -0500

    yet more deps missing for the cloud build.4

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,5 @@
-const { DynamoDB } = require('aws-sdk')
 const localDynamo = require('local-dynamo')
+const { DynamoDB, Credentials, SharedIniFileCredentials } = require('aws-sdk')
 
 let dynamoLocal
 
@@ -7,10 +7,16 @@ module.exports = async () => {
   // start local Dynamo Svc
   // and set the internals of the Entity framework to use the locally configured reader/writer
   const port = 4567
+
   dynamoLocal = await localDynamo.launch(undefined, port)
+
+  const credentials = process.env.AWS_KEY && process.env.AWS_SECRET
+    ? new Credentials({ accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET })
+    : new SharedIniFileCredentials({ profile: 'default' })
+
   console.log('...started dynamo local svc on pid: ', dynamoLocal.pid)
 
-  const dyn = new DynamoDB({ region: 'us-east-1', endpoint: `http://localhost:${port}` })
+  const dyn = new DynamoDB({ region: 'us-east-1', credentials, endpoint: `http://localhost:${port}` })
   await dyn.createTable({
     TableName: 'emooreAppTable',
     AttributeDefinitions: [
