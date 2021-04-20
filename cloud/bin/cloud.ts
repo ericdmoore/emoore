9325a0157d25269b65c83012001d4541bdb425c0
Eric Moore <eric.d.moore@gmail.com>
2021-04-20 14:24:14 -0500

    cloud updates

--- cloud/bin/cloud.ts
+++ cloud/bin/cloud.ts
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CloudStack } from '../lib/cloud-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { CloudStack } from '../lib/cloud-stack'
 
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 const processenv = config()
 
-;(async ()=>{
-
-  const app = new cdk.App();
+;(async () => {
+  const app = new cdk.App()
   new CloudStack(app, 'CloudStack', {
-    stackName:'emoore-app',
-    description:'An application that runs on emoo.re'
+    stackName: 'emoore-app',
+    description: 'An application that runs on emoo.re'
     /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-  });
-
+  })
 })().catch(console.error)
